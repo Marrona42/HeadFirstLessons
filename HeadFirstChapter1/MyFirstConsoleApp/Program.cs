8108c826b2c96c44b57cf2d3f5194b04296a0164
Marrona42 <economy42@vk.com>
2023-11-26 16:49:32 +0700

    Add Chapter 2

---
+++ HeadFirstChapter1/MyFirstConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TryAnIf();
+            TrySomeLoops();
+            TryAnIfElse();
+        }
+
+        private static void TryAnIfElse()
+        {
+            int someValue = 4;
+            string name = "Bobo Jr.";
+
+            if((someValue == 3) && (name == "Joe"))
+            {
+                Console.WriteLine("x is 3 and the name is Joe");
+            }
+            Console.WriteLine("this line runs no matter what");
+        }
+
+        private static void TrySomeLoops()
+        {
+            int x = 5;
+            if (x == 10)
+            {
+                Console.WriteLine("x mus be 10");
+            }
+            else
+            {
+                Console.WriteLine("x isn't 10");
+            }
+        }
+
+        private static void TryAnIf()
+        {
+            int count = 0;
+            while (count < 10)
+            {
+                count = count + 1;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                count = count - 1;
+            }
+            Console.WriteLine("The answer is " + count);    
+        }
+    }
+} 
